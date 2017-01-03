f2b63cafc1a30cad2893f37dc7afc340919137a3
Anna Kviese <anna.kviese@gmail.com>
2017-01-03 12:03:40 -0800

    initial commit

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,49 @@
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+@Component ({
+    selector: 'my-app',
+    template: `<h1>{{title}}</h1> 
+               <ul>
+                    <li *ngFor="let province of provinces">
+                        <p>{{province.name}} </p>
+                        <p>{{province.taxBrackets}}</p>
+                    </li>
+                </ul>`
+})
+class AppComponent { 
+    title = "TFSA vs RRSP"
+    provinces =  [
+        {
+        'id'   : 1,    
+        'name' : 'Ontario', 
+        'taxBrackets' : [
+                         {'name': '10k to 50k', 'id': 1},
+                         {'name': '50k to 80k', 'id': 2},
+                         {'name': '80k to 120k', 'id': 3},
+                         {'name': '10k to 50k', 'id': 4} 
+                        ]
+                        },
+        {
+        'id'   : 1,    
+        'name' : 'British Columbia', 
+        'taxBrackets' : [  
+                         {'name': '10k to 50k', 'id': 1},
+                         {'name': '50k to 80k', 'id': 2},
+                         {'name': '80k to 120k', 'id': 3},
+                         {'name': '10k to 50k', 'id': 4} 
+                        ]                        
+                        },
+    ]
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+@NgModule ({
+    declarations: [ AppComponent ],
+    imports: [ BrowserModule ],
+    bootstrap: [ AppComponent ]
+})
+class AppModule { }
+
+platformBrowserDynamic() 
+    .bootstrapModule(AppModule);
