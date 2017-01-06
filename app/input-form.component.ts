ab84529d8e65b1d1c54339cbd962233bd59efdc6
Anna Kviese <anna.kviese@gmail.com>
2017-01-05 21:47:54 -0800

    updated tax paid formula

--- app/input-form.component.ts
+++ app/input-form.component.ts
@@ -19,12 +19,12 @@ export class InputFormComponent {
        this.model.tsfaAfterTax = this.model.DepositAmount * (1 - this.model.MarginalTaxRate);
        this.model.rrspAfterTax =  this.model.DepositAmount;
        //calculates Future Value in Todays Dollars
-       this.model.rateOfReturn = (1 + this.model.ROI)/ (1 + this.model.Inflation) - 1;
+       this.model.rateOfReturn = ((1 + this.model.ROI)/ (1 + this.model.Inflation)) - 1;
        this.model.tsfaFutureValue = Math.round(this.model.tsfaAfterTax * Math.pow((1 + this.model.rateOfReturn),this.model.Years));
        this.model.rrspFutureValue =  Math.round(this.model.rrspAfterTax * Math.pow((1 + this.model.rateOfReturn),this.model.Years));
        //calculates Tax Paid Upon Withdrawal
        this.model.tsfaTaxPaid = 0;
-       this.model.rrspTaxPaid = Math.round(this.model.rrspFutureValue * this.model.AvRetirenmentTax);
+       this.model.rrspTaxPaid = Math.round(this.model.rrspFutureValue) * this.model.AvRetirenmentTax;
        //calculates Future Value at the End of the period
        this.model.tsfaEndValue = Math.round(this.model.tsfaFutureValue);
        this.model.rrspEndValue = Math.round(this.model.rrspFutureValue - this.model.rrspTaxPaid); 
